b7cba4af14b3f988e9e40be09faeeb89999c4311
fkfkfk999 <fkfkfk9@daum.net>
2021-06-15 22:43:20 +0900

    테스트 프로젝트 다시 생성

--- testBoard/src/main/java/egovframework/learn/testboard/service/TestBoardService.java
+++
@@ -1,9 +0,0 @@
-package egovframework.learn.testboard.service;
-
-import java.util.List;
-
-import egovframework.learn.testboard.model.TestBoardVO;
-
-public interface TestBoardService {
-	public List<TestBoardVO> getTestBoardAllList() throws Exception;
-}
